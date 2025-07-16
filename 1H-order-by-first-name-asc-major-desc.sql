9f54a6a9daec15effcb9c120f1720d8e8372530b
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-07-16 16:33:46 +0530

    Create 1H-order-by-first-name-asc-major-desc.sql

---
+++ 1H-order-by-first-name-asc-major-desc.sql
@@ -0,0 +1,7 @@
+Write a SQL query to print all Student details from Student table order by FIRST_NAME Ascending and MAJOR Subject descending .
+
+>> select * from students order by FIRST_NAME, MAJOR desc;
+
+Explanation:
+
+The ORDER BYclause sorts records first by FIRST_NAME in ascending order, then by MAJOR in descending order.
