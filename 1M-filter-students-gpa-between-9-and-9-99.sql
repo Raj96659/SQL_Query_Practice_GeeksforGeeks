6f52280296c0976a4266d4f6a3e021db7fd61948
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-07-16 16:43:16 +0530

    Create 1M-filter-students-gpa-between-9-and-9-99.sql

---
+++ 1M-filter-students-gpa-between-9-and-9-99.sql
@@ -0,0 +1,8 @@
+Write an SQL query to print details of the Students whose GPA lies between 9.00 and 9.99.
+
+>> select * from students where GPA between 9.00 and 9.99;
+
+Explanation:
+
+The BETWEEN operator retrieves rows where the GPA falls within the inclusive range of 9.00 to 9.99. 
+This query outputs the students who scored high GPAs in the given range, showcasing the top performers.
