b65f443ed15a3f3ae90813187efbcd224c7b672e
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-07-16 16:10:05 +0530

    Create 1B-fetch-unique-major-subjects.sql

---
+++ 1B-fetch-unique-major-subjects.sql
@@ -0,0 +1,7 @@
+Write a SQL query to fetch unique values of MAJOR Subjects from Student table.
+
+>>  select distinct(MAJOR) from students;
+
+Explanation:
+
+The DISTINCTkeyword ensures that only unique values from the MAJOR column are retrieved, removing duplicates and providing a clean list of all subjects.
